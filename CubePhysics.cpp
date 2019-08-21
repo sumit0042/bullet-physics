a4af6899eac09428fdc572b34f35dcff843f3f0f
Sumit Kumar <sumit.kumar3@ubisoft.com>
2019-08-21 21:04:50 +0530

    first

---
+++ CubePhysics.cpp
@@ -0,0 +1,146 @@
+#include<GL/glew.h>     // use this before GLFW
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<tuple>
+#include<Windows.h>
+
+#include "Shader.h"
+#include "Camera.h"
+#include "Data.h"
+
+#include "C:\Users\Skumar3\source\repos\libraries\bullet3-master\src\btBulletDynamicsCommon.h"
+
+using namespace std;
+
+/*=======================================================================================================================
+			CAMERA AND INPUTS
+=======================================================================================================================*/
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+Camera camera(glm::vec3(0.0f, 0.0f, 0.8f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+
+/*=======================================================================================================================
+			PROGRAM AND DATA BINDING
+=======================================================================================================================*/
+
+int main()
+{
+	auto collisionConfiguration = new btDefaultCollisionConfiguration();
+	auto dispatcher = new btCollisionDispatcher(collisionConfiguration);
+	auto overlappingPairCache = new btDbvtBroadphase();
+	auto solver = new btSequentialImpulseConstraintSolver;
+	auto  dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+
+	GLFWwindow* window;
+
+	// Initialize the library 
+	if (!glfwInit())
+		return -1;
+
+	// Create a windowed mode window and its OpenGL context 
+	window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	// Make the window's context current 
+	glfwMakeContextCurrent(window);
+
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	glewInit();
+
+	dynamicsWorld->setGravity({ 0,-10,0 });
+
+	Cube cube(dynamicsWorld);
+	//Cube cuba;
+	//cube->m_g = g;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glEnable(GL_DEPTH_TEST);
+		float currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+
+		processInput(window);
+
+		glClearColor(0.5f, 0.5f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		cube.draw(camera);
+		//cuba.draw(camera);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		dynamicsWorld->stepSimulation(1.f / 100.f, 100);
+	}
+
+	glUseProgram(0);
+	glfwTerminate();
+	return 0;
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//----------------------------------------------------------------------------------------------------
---
+++ Data.h
@@ -0,0 +1,210 @@
+#pragma once
+#include<GL/glew.h>     // use this before GLFW
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "Camera.h"
+
+#include "C:\Users\Skumar3\source\repos\libraries\bullet3-master\src\btBulletDynamicsCommon.h"
+
+const int no_rects = 6;
+
+
+GLuint create_program()
+{
+	GLuint program = glCreateProgram();
+	shaderAttach(program, GL_VERTEX_SHADER, vertexShaderSrc);
+	shaderAttach(program, GL_FRAGMENT_SHADER, fragmentShaderSrc);
+	glLinkProgram(program);
+	glValidateProgram(program);
+	return program;
+}
+
+
+void bind_vert_n_texel(GLuint program)
+{
+	GLuint posAttrib = glGetAttribLocation(program, "pos");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+
+	posAttrib = glGetAttribLocation(program, "texco");
+	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+}
+
+
+struct Vertex
+{
+	float x, y, z;
+	float tx = 0.0; float ty = 0.0;
+};
+
+
+class Cube
+{
+public:
+	float mass = 1;
+	btRigidBody* m_rigid = 0;
+	GLuint m_g;
+	GLuint program[no_rects];
+	glm::mat4x3 m_faces[no_rects];
+	Vertex m_c[no_rects][4];
+
+	Cube(btDiscreteDynamicsWorld* world)
+	{
+		m_bind_buffer();
+
+		for (size_t i = 0; i < no_rects; i++)
+		{
+			program[i] = create_program();
+		}
+	}
+
+	~Cube()
+	{
+		for (size_t i = 0; i < no_rects; i++)
+		{
+			glDeleteProgram(program[i]);
+		}
+		glDeleteVertexArrays(1, &m_g);
+	}
+
+	void bind_static_attribs()
+	{
+		for (size_t i = 0; i < no_rects; i++)
+		{
+			glUseProgram(program[i]);
+			bind_vert_n_texel(program[i]);
+			//bind_texture(program[i]);
+		}
+	}
+
+	void m_bind_buffer()
+	{
+		glGenBuffers(1, &m_g);
+		this->create_faces_data();
+		glBindBuffer(GL_ARRAY_BUFFER, this->m_g);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(this->m_c), this->m_c, GL_STATIC_DRAW);
+	}
+
+	void draw(Camera camera)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, this->m_g);
+		bind_static_attribs();
+		
+		for (size_t i = 0; i < no_rects; i++)
+		{
+			glUseProgram(program[i]);
+
+			glm::mat4 model = glm::mat4(1.0f);
+			model[3][0] = 0.6f;
+			GLuint modelLoc = glGetUniformLocation(program[i], "model");
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+			glm::mat4 view = camera.GetViewMatrix();
+			GLint viewLoc = glGetUniformLocation(program[i], "view");
+			glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+			glm::mat4 pro = glm::mat4(1.0f);
+			pro = glm::perspective(glm::radians(camera.Zoom), 1280.0f / 720.0f, 0.01f, 100.0f);
+			GLint projectionLoc = glGetUniformLocation(program[i], "projection");
+			glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(pro));
+
+			glDrawArrays(GL_TRIANGLE_FAN, i * 4, 4);
+			glUseProgram(0);
+		}
+	}
+
+	void create_faces_data()
+	{
+		glm::mat4x3 frontFace = {
+			{-0.2f,-0.2f, 0.2f},
+			{-0.2f, 0.2f, 0.2f},
+			{ 0.2f, 0.2f, 0.2f},
+			{ 0.2f,-0.2f, 0.2f}
+		};
+
+		glm::mat4x3 backFace = {
+			{-0.2f,-0.2f,-0.2f},
+			{-0.2f, 0.2f,-0.2f},
+			{ 0.2f, 0.2f,-0.2f},
+			{ 0.2f,-0.2f,-0.2f},
+		};
+
+		glm::mat4x3 topFace = {
+			{ 0.2f, 0.2f,-0.2f},
+			{-0.2f, 0.2f,-0.2f},
+			{-0.2f, 0.2f, 0.2f},
+			{ 0.2f, 0.2f, 0.2f}
+		};
+
+		glm::mat4x3 bottomFace = {
+			{-0.2f,-0.2f, 0.2f},
+			{-0.2f,-0.2f,-0.2f},
+			{ 0.2f,-0.2f,-0.2f},
+			{ 0.2f,-0.2f, 0.2f},
+		};
+
+		glm::mat4x3 rightFace = {
+			{ 0.2f, 0.2f, 0.2f},
+			{ 0.2f, 0.2f,-0.2f},
+			{ 0.2f,-0.2f,-0.2f},
+			{ 0.2f,-0.2f, 0.2f},
+		};
+
+		glm::mat4x3 leftFace = {
+			{-0.2f, 0.2f, 0.2f},
+			{-0.2f,-0.2f, 0.2f},
+			{-0.2f,-0.2f,-0.2f},
+			{-0.2f, 0.2f,-0.2f},
+		};
+
+		m_faces[0] = frontFace;
+		m_faces[1] = backFace;
+		m_faces[2] = leftFace;
+		m_faces[3] = bottomFace;
+		m_faces[4] = rightFace;
+		m_faces[5] = topFace;
+
+		for (size_t i = 0; i < no_rects; i++)
+		{
+			for (size_t j = 0; j < 4; j++)
+			{
+				m_c[i][j].x = m_faces[i][j].x;
+				m_c[i][j].y = m_faces[i][j].y;
+				m_c[i][j].z = m_faces[i][j].z;
+				switch (j)
+				{
+				case 0:
+				{
+					m_c[i][j].tx = 0.0f;
+					m_c[i][j].ty = 0.0f;
+					break;
+				}
+				case 1:
+				{
+					m_c[i][j].tx = 0.0f;
+					m_c[i][j].ty = 1.0f;
+					break;
+				}
+				case 2:
+				{
+					m_c[i][j].tx = 1.0f;
+					m_c[i][j].ty = 1.0f;
+					break;
+				}
+				case 3:
+				{
+					m_c[i][j].tx = 1.0f;
+					m_c[i][j].ty = 0.0f;
+					break;
+				}
+				default:
+					break;
+				}
+			}
+		}
+	}
+};
